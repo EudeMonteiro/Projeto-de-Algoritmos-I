dac27a144e4feff1a04e1a7e85e5f28f006959bb
EudeMonteiro <73922540+EudeMonteiro@users.noreply.github.com>
2021-07-30 22:35:54 -0300

    Update Palindromo.c

--- Palindromo.c
+++ Palindromo.c
@@ -3,26 +3,44 @@
 #include <stdio.h>
 #include <string.h>
 
-void Palindromo(char str[])
+int Palindromo(char str[])
 {
   int x = 0;
   int y = strlen(str) - 1;
+  int rt = 0;
 
   while (y > x)
   {
     if (str[x++] != str[y--])
     {
-      printf("%s não é um palíndromo.\n", str);
-      return;
+      return rt;
     }
   }
-  printf("%s é um palíndromo. \n", str);
+  rt++;
+  return rt;
 }
 
 int main() {
+  
+  int k;
+  char palavras[][100] = {
+    "1132311",
+    "abbcdcbba",
+    "projeto",
+    "algoritmo",
+    "arara",
+    ""
+  };
 
-  Palindromo("1132311");
-  Palindromo("abbcdcbba");
-  Palindromo("algoritmo");
+  for (k=0; palavras[k][0];k++) {
+    printf("%s", palavras[k]);
+    if (Palindromo(palavras[k])){
+      printf(" é palindromo. \n");
+    }
+
+    else {
+      printf(" não é palíndromo. \n");
+    }
+  }
   return 0;
 }
